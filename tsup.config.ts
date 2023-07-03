67d5bd63df5f482af59abeadb9eedb5b3ab1968b
lucaslos <lucas@sugestly.com>
2023-07-03 15:17:29 -0300

    fix react utils

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import pkg from './package.json'
 import { writeFileSync } from 'fs'
 
 export default defineConfig({
-  entry: ['src/evtmitter.ts', 'src/evtmitter/react.ts'],
+  entry: ['src/evtmitter.ts', 'src/react.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   esbuildOptions(options) {
--- tsup.test.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['src/runcheck.ts', 'src/autofixable.ts'],
-  clean: true,
-  outDir: 'dist-test',
-  format: ['esm'],
-})
